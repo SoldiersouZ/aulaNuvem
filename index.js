ff4df44c3d5264d201566be50265a545602e1947
aluno <e023aluno@outlook.com>
2026-03-27 17:44:03 -0300

    alterado o projeto

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send("Servidor rodando na nuvem 🚀");
 })
+app.get('/teste', (req, res)=>{
+    res.send("Rota de teste funcionando 🚀");
+})
 
 const PORT = process.env.PORT || 3000;
 
